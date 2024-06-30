c6df35c87ddcdbfd97b7f54bb2686a8c3b6f5b25
sahana29-web <sahanam2912@gmail.com>
2024-06-30 22:41:25 +0530

    Add files via upload

---
+++ project/cards.js
@@ -0,0 +1,50 @@
+export const CARDDATA = [
+  {
+    name: "Mumbai",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Mumbai_Aug_2018_%2843397784544%29.jpg/1200px-Mumbai_Aug_2018_%2843397784544%29.jpg",
+    desc: "Mumbai is a densely populated city on India west coast. A financial center, it's India's largest city. On the Mumbai Harbour waterfront",
+    price: 15000
+  },
+  {
+    name: "Bangalore",
+    image: "https://static.thehosteller.com/hostel/images/Cover%20(1).jpg/Cover%20(1)-1690261803767.jpg",
+    desc: "Bengaluru is the capital of India's southern Karnataka state. The center of India's high-tech industry, the city is also known for its parks and nightlife",
+    price: 10000
+  },
+  {
+    name: "Goa",
+    image: "https://images.cnbctv18.com/wp-content/uploads/2022/05/Goa-shutterstock-1000x573.jpg",
+    desc: "Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961",
+    price: 20000
+  },
+  {
+    name: "Jaipur",
+    image: "https://static.toiimg.com/photo/105088670.cms",
+    desc: "Jaipur is the capital of India Rajasthan state. It evokes the royal family that once ruled the region and that, in 1727, founded what is now called the Old City, or “Pink City” ",
+    price: 25000
+  },
+  {
+    name: "Varanasi",
+    image: "https://ihplb.b-cdn.net/wp-content/uploads/2023/03/The-Changing-Landscape-of-Indian-cities-Varanasi.jpg",
+    desc: "Varanasi is a city in the northern Indian state of Uttar Pradesh dating to the 11th century B.C. Regarded as the spiritual capital of India",
+    price: 30000
+  },
+  {
+    name: "Ayodhya",
+    image: "https://etimg.etb2bimg.com/photo/107039405.cms",
+    desc: "Ayodhya is a city situated on the banks of the Sarayu river in the Indian state of Uttar Pradesh. ",
+    price: 35000
+  },
+  {
+    name: "Delhi",
+    image: "https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg",
+    desc: "Delhi, India capital territory, is a massive metropolitan area in the country north. In Old Delhi, a neighborhood dating to the 1600s",
+    price: 12000
+  },
+  {
+    name: "Shimla",
+    image: "https://static.toiimg.com/thumb/msid-102383896,width-748,height-499,resizemode=4,imgsize-238460/.jpg",
+    desc: "Shimla is the capital of the northern Indian state of Himachal Pradesh, in the Himalayan foothills. Once the summer capital of British India",
+    price: 15000
+  }
+];
